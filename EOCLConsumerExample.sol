9384deb90e7070535eac629168b62c6752667143
Ron Turetzky <ron@eoracle.io>
2024-06-16 11:57:19 +0300

    init: adding examples

---
+++ EOCLConsumerExample.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+interface IEOFeedAdapter {
+    function getPairSymbol() external view returns (uint16);
+    function decimals() external view returns (uint8);
+    function description() external view returns (string memory);
+    function version() external view returns (uint256);
+
+    // getRoundData and latestRoundData both raise "No data present"
+    // if they do not have data to report, instead of returning unset values
+    // which could be misinterpreted as actual reported values.
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+    function latestRoundData()
+        external
+        view
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
+
+contract EOCLConsumerExample {
+    IEOFeedAdapter public _feedAdapter;
+     /**
+     * Network: Holesky
+     * EOFeedAdapter: 0xDD8387185C9e0a173702fc4a3285FA576141A9cd
+     * Feed Symbol: BTC 
+     */
+    constructor() {
+        _feedAdapter = IEOFeedAdapter(0xDD8387185C9e0a173702fc4a3285FA576141A9cd);
+    }
+
+    function getPrice() external view returns (int256 answer) {
+        (, answer,,,) = _feedAdapter.latestRoundData();
+    }
+    
+    function usePrice() external {
+        int256 answer = getPrice();  
+        // Do something 
+        // .............
+    }
+}
---
+++ EOConsumerExample.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IEOFeedManager {
+    struct PriceFeed {
+        uint256 value;
+        uint256 timestamp;
+    }
+    function getLatestPriceFeed(uint16 symbol) external view returns (PriceFeed memory);
+    function getLatestPriceFeeds(uint16[] calldata symbols) external view returns (PriceFeed[] memory);
+}
+
+contract EOConsumerExample {
+    IEOFeedManager public _feedManager;
+    
+    /**
+     * Network: Holesky
+         * FeedManager: 0x723BD409703EF60d6fB9F8d986eb90099A170fd0
+     */
+    constructor() {
+        _feedManager = IEOFeedManager(0x723BD409703EF60d6fB9F8d986eb90099A170fd0);
+    }
+
+    // Example for using IEOFeedManager.getLatestPriceFeed with a single symbol.
+    function getPrice() external view returns (IEOFeedManager.PriceFeed memory) {
+        return _feedManager.getLatestPriceFeed(1); // 1 is the id for BTC:USD
+    }
+    
+    function usePrice() external {
+        IEOFeedManager.PriceFeed memory data = this.getPrice(); 
+        uint256 value = data.value; 
+        uint256 timestamp = data.timestamp; 
+        // Do something 
+        // .............
+    }
+}
